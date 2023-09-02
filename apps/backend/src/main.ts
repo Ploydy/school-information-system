dc58ce7da2719261a11d11a9dd10d9e804a37a87
Ploydy <Lfecarballo20@gmail.com>
2023-09-02 16:07:22 +0800

    Added my project

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
 
