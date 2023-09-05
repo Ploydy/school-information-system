24dbb963a1d997f27c94ddc6e3d242243c8e5f54
Ploydy <Lfecarballo20@gmail.com>
2023-09-06 01:52:01 +0800

    added react hook form and working on login layout

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,24 +1,40 @@
+import { useForm } from 'react-hook-form';
 
+
+type FormData = {
+  email: string,
+  password: string
+};
 
 export default function Login() {
 
- /*  const { 
-    register, 
-    handleSubit, 
-    formState: { errors }, 
-  } = useForm(); */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const handleLogin = (data: FormData) => {
+    console.log(data);
+  };
 
   return (
     <section>
       <div>
-      <input
-      placeholder="Email address"
-      type="email"
-      /* {...register} */
-      />
-      
-
+        <input
+          placeholder="Email address"
+          type="email"
+          {...register('email')}
+        />
       </div>
+      <div>
+        <input
+          placeholder="Password"
+          type="password"
+          {...register('password')}
+        />
+      </div>
+      <button onClick={handleSubmit(handleLogin)}></button>
     </section>
   )
 };
