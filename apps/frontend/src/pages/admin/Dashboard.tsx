6894dab668139ce2717cf149299e5957a41780e8
Ploydy <Lfecarballo20@gmail.com>
2023-09-13 22:19:20 +0800

    redux toolkit working

--- apps/frontend/src/pages/admin/Dashboard.tsx
+++ apps/frontend/src/pages/admin/Dashboard.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useAppDispatch, useAppSelector } from '../../state/hooks';
+import { increment, decrement } from '../../state/slices/counterSlice';
 
 function Dashboard() {
+
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const handleIncrement = () => {
+    dispatch(increment())
+  };
+  const handleDecrenent = () => {
+    dispatch(decrement())
+  };
 
   const handleLogout = () => {
     console.log('someone clicked logout!');
@@ -9,7 +20,10 @@ function Dashboard() {
   return (
     <div>
       Dashboard
+      {count}
     <button onClick={handleLogout}>logout</button>
+    <button onClick={handleIncrement}>increment</button>
+    <button onClick={handleDecrenent}>decrement</button>
     </div>
   )
 }
