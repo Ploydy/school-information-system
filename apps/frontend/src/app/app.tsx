1f1df13bc140e6398c6371d8b5468d0c9ad05d49
Ploydy <Lfecarballo20@gmail.com>
2023-09-22 17:58:45 +0800

    working loader

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect, useState } from 'react';
 import router from '../router';
 import styles from './app.module.css';
 import { RouterProvider } from 'react-router-dom';
+import Loader from '../common/Loader';
 
 
 
 export function App() {
-  return (
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+  
+  return loading ? (
+    <Loader />
+  ) : (
     <div>
       <RouterProvider router={router} />
     </div>
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
+/* import './styles.css'; */
 import App from './app/app';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
